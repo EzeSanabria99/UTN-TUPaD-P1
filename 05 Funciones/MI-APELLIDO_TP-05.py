6b87588c3dd6aec4a733fab964caddb6530f2e9a
Ezequiel Franco Sanabria <127760298+EzeSanabria99@users.noreply.github.com>
2025-04-24 02:03:59 -0300

    Update MI-APELLIDO_TP-05.py

--- 05 Funciones/MI-APELLIDO_TP-05.py
+++ 05 Funciones/MI-APELLIDO_TP-05.py
@@ -1,3 +1,3 @@
-Ezequiel Franco Sanabria
+#Ezequiel Franco Sanabria
 
-Por el momento, solo podre publicar la unidad 5.1, cuando vuelvan las unidades originales publicare la otra parte
+#Por el momento, solo podre publicar la unidad 5.1, cuando vuelvan las unidades originales publicare la otra parte
