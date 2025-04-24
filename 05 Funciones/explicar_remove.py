4311051a0c56d6ce7cf13a48f12538ff95e9499d
Ezequiel Franco Sanabria <127760298+EzeSanabria99@users.noreply.github.com>
2025-04-24 01:23:05 -0300

    Add files via upload

---
+++ 05 Funciones/explicar_remove.py
@@ -0,0 +1,10 @@
+numeros = [8, 15, 3, 22, 7]
+numeros.remove(max(numeros))
+print(numeros)
+
+# Hay una lista de numeros que se encuentran [8, 15, 3, 22, 7]
+# Después hay una funcion numeros.remove que eliminaria el indice que señale
+
+# pero en vez de poner el indice, pongo max(numeros)
+#que busca el valor mayor de estos numeros enteros como sería el 22 para eliminarlo.
+#probe en cambiar max por min, para que borre el valor más chico y así comprenderlo mejor.
