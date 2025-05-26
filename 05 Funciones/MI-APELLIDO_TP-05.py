e46ee3c46f788d7ecf802f64f4af9f62ea1a7409
Ezequiel Franco Sanabria <127760298+EzeSanabria99@users.noreply.github.com>
2025-05-25 23:57:57 -0300

    Update MI-APELLIDO_TP-05.py

--- 05 Funciones/MI-APELLIDO_TP-05.py
+++ 05 Funciones/MI-APELLIDO_TP-05.py
@@ -1,3 +1,5 @@
 #Ezequiel Franco Sanabria
 
 #Por el momento, solo podre publicar la unidad 5.1, cuando vuelvan las unidades originales publicare la otra parte
+
+#Ahora, hice el resto de la unidad 5. Los ejercicios de la unidad Funciones, arrancan con el nombre "fun" o "funcion" y luego el nombre del ejercicio con .py .
